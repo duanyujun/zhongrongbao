5c9c32c8c62a2bb7ef1810e2ab33bee56d541471
mal <xihayouyi@126.com>
2015-07-31 16:01:18 +0800

    漏下的

---
+++ zhongrongbaoTests/zhongrongbaoTests.m
@@ -0,0 +1,34 @@
+//
+//  zhongrongbaoTests.m
+//  zhongrongbaoTests
+//
+//  Created by mal on 14-8-12.
+//  Copyright (c) 2014年 cnsoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface zhongrongbaoTests : XCTestCase
+
+@end
+
+@implementation zhongrongbaoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
